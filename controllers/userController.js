b675e592e04be24449d259e10f578bb0a2f03de6
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 04:49:12 +0300

    create updateUser function in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,50 +3,79 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        if (!name || !email || !password) return res.status(400).json({ message: "Missing some required data" });
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password)
+      return res.status(400).json({ message: "Missing some required data" });
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) return res.status(409).json({ message: "User already exists" });
-        if (password.length < 6) return res.status(400).json({ message: "Weak password, please choose stronger one" });
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(409).json({ message: "User already exists" });
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ message: "Weak password, please choose stronger one" });
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            name,
-            email,
-            password: hashedPassword,
-        });
-        await newUser.save();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
 
-        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
-        res.status(201).json({ user: newUser, token });
-    } catch (error) {
-        res.status(500).json({ message: "Error registering user" });
-    }
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
+    res.status(201).json({ user: newUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user" });
+  }
 };
 
 const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) return res.status(400).json({ message: "Missing some required data" });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ message: "Missing some required data" });
 
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Invalid password" });
-        }
-
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
-        res.status(200).json({ user, token });
-    } catch (error) {
-        res.status(500).json({ message: "Error logging in" });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in" });
+  }
 };
 
-module.exports = { register, login };
+const updateUser = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const userId = req.user.userId;
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (name) user.name = name;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
+module.exports = { register, login, updateUser };
