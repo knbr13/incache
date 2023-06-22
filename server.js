361754790a06d380b525ec6da76f9671ec09e351
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 03:48:02 +0300

    run the server and listen on the port defined in .env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log("the server is up, and listening on port", process.env.PORT);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log("the server is up, and listening on port", PORT);
 });
