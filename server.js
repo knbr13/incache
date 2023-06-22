10abf2e7be9ab35d70ee82378498a569e3b3efdd
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 04:35:33 +0300

    use express.json middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 
 const userRoutes = require("./routes/userRoutes");
 app.use("/user", userRoutes);
