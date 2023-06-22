2170f8c9eb3824aea3005401b3f25ad2a5948fcb
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 04:22:59 +0300

    create login function in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,27 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "Missing some required data" });
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+        res.status(200).json({ user, token });
+    } catch (error) {
+        res.status(500).json({ message: "Error logging in" });
+    }
+};
+
+module.exports = { register, login };
