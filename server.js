4ff792b068a00b6ec486f11bb355d4e0c3e7a402
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 04:31:28 +0300

    use user routes in the main router

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+app.use("/user", userRoutes);
+
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(PORT, () => {
-        console.log("the server is up, and listening on port", PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("the server is up, and listening on port", PORT);
+  });
 });
