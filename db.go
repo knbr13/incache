4350b9a22474431fa1e21719cc62e2495ecf7c7d
knbr13 <abdullahalaadine63@gmail.com>
2024-03-26 02:54:38 +0200

    add constructor function to DB type

--- db.go
+++ db.go
@@ -6,3 +6,10 @@ type DB[K comparable, V any] struct {
 	m  map[K]V
 	mu sync.RWMutex
 }
+
+func New[K comparable, V any]() *DB[K, V] {
+	return &DB[K, V]{
+		m:  make(map[K]V),
+		mu: sync.RWMutex{},
+	}
+}
