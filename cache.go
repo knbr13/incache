f6beb70a88cf893843d1cca92d1e69a503a86c51
knbr13 <abdullahalaadine63@gmail.com>
2024-03-31 15:15:56 +0300

    create Cache interface

---
+++ cache.go
@@ -0,0 +1,43 @@
+package inmemdb
+
+import "time"
+
+// Cache represents a generic caching interface for key-value pairs.
+// Different cache implementations can be created by implementing this interface.
+type Cache[K comparable, V any] interface {
+	// Get retrieves the value associated with the given key from the cache.
+	// It returns the value and true if the key exists in the cache, otherwise, it returns the zero value of the value type and false.
+	Get(K) (V, bool)
+
+	// Set adds or updates the key-value pair in the cache without setting an expiration time.
+	// If the key already exists, its value will be overwritten with the new value.
+	Set(K, V)
+
+	// SetWithTimeout adds or updates the key-value pair in the cache with an expiration time.
+	// If the timeout duration is zero or negative, the key-value pair will not have an expiration time.
+	SetWithTimeout(K, V, time.Duration)
+
+	// NotFoundSet adds a key-value pair to the cache if the key does not already exist and returns true.
+	// Otherwise, it does nothing and returns false.
+	NotFoundSet(K, V) bool
+
+	// NotFoundSetWithTimeout adds a key-value pair to the cache with an expiration time if the key does not already exist and returns true.
+	// If the timeout is zero or negative, the key-value pair will not have an expiration time.
+	// If expiry is disabled, it behaves like NotFoundSet.
+	NotFoundSetWithTimeout(K, V, time.Duration) bool
+
+	// Delete removes the key-value pair associated with the given key from the cache.
+	Delete(K)
+
+	// TransferTo transfers all key-value pairs from the source cache to the provided destination cache.
+	TransferTo(*DB[K, V])
+
+	// CopyTo copies all key-value pairs from the source cache to the provided destination cache.
+	CopyTo(*DB[K, V])
+
+	// Keys returns a slice containing the keys of the cache in arbitrary order.
+	Keys() []K
+
+	// Count returns the number of key-value pairs in the cache.
+	Count() int
+}
