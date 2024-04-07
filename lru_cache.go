14a9e6e67ba69654b5e96d1a90cf2c2f8cbbea45
knbr13 <abdullahalaadine63@gmail.com>
2024-04-07 18:56:59 +0300

    add set helper func

--- lru_cache.go
+++ lru_cache.go
@@ -5,14 +5,15 @@ import (
 	"time"
 )
 
-type lruItem[V any] struct {
-	value    *list.Element
+type lruItem[K comparable, V any] struct {
+	key      K
+	value    V
 	expireAt *time.Time
 }
 
 type LRUCache[K comparable, V any] struct {
 	baseCache
-	m            map[K]lruItem[V] // where the key-value pairs are stored
+	m            map[K]*list.Element // where the key-value pairs are stored
 	evictionList *list.List
 }
 
@@ -21,7 +22,7 @@ func newLRU[K comparable, V any](cacheBuilder *CacheBuilder[K, V]) *LRUCache[K, 
 		baseCache: baseCache{
 			size: cacheBuilder.size,
 		},
-		m:            make(map[K]lruItem[V]),
+		m:            make(map[K]*list.Element),
 		evictionList: list.New(),
 	}
 }
@@ -35,13 +36,13 @@ func (c *LRUCache[K, V]) Get(k K) (v V, b bool) {
 		return
 	}
 
-	if item.expireAt != nil && item.expireAt.Before(time.Now()) {
+	if item.Value.(*lruItem[K, V]).expireAt != nil && item.Value.(*lruItem[K, V]).expireAt.Before(time.Now()) {
 		delete(c.m, k)
-		c.evictionList.Remove(item.value)
+		c.evictionList.Remove(item)
 		return
 	}
 
-	return item.value.Value.(V), true
+	return item.Value.(*lruItem[K, V]).value, true
 }
 
 func (c *LRUCache[K, V]) GetAll() map[K]V {
@@ -50,18 +51,42 @@ func (c *LRUCache[K, V]) GetAll() map[K]V {
 
 	m := make(map[K]V)
 	for k, v := range c.m {
-		if v.expireAt == nil || !v.expireAt.Before(time.Now()) {
-			m[k] = v.value.Value.(V)
+		if v.Value.(*lruItem[K, V]).expireAt != nil && v.Value.(*lruItem[K, V]).expireAt.Before(time.Now()) {
+			m[k] = v.Value.(*lruItem[K, V]).value
 		}
 	}
 
 	return m
 }
 
+func (c *LRUCache[K, V]) set(k K, v V, exp *time.Duration) {
+	item, ok := c.m[k]
+	tm := time.Now().Add(*exp)
+	if ok {
+		item.Value.(*lruItem[K, V]).value = v
+		item.Value.(*lruItem[K, V]).expireAt = &tm
+		c.evictionList.MoveToFront(item)
+	} else {
+		if len(c.m) == int(c.size) {
+			c.evict(1)
+		}
+
+		c.m[k] = &list.Element{
+			Value: &lruItem[K, V]{
+				key:      k,
+				value:    v,
+				expireAt: &tm,
+			},
+		}
+		c.evictionList.PushFront(c.m[k])
+	}
+}
+
 func (c *LRUCache[K, V]) evict(i int) {
 	for j := 0; j < i; j++ {
 		if b := c.evictionList.Back(); b != nil {
 			c.evictionList.Remove(b)
+			delete(c.m, b.Value.(lruItem[K, V]).key)
 		} else {
 			return
 		}
