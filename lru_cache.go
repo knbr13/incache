52bf58579152e3e44a867dbf1b3ff23ea0bc739c
knbr13 <abdullahalaadine63@gmail.com>
2024-04-07 15:48:10 +0300

    add newLRU constructor function

--- lru_cache.go
+++ lru_cache.go
@@ -5,13 +5,23 @@ import (
 	"time"
 )
 
+type lruItem[V any] struct {
+	value    *list.Element
+	expireAt *time.Time
+}
+
 type LRUCache[K comparable, V any] struct {
 	baseCache
 	m            map[K]lruItem[V] // where the key-value pairs are stored
 	evictionList *list.List
 }
 
-type lruItem[V any] struct {
-	value    *list.Element
-	expireAt *time.Time
+func newLRU[K comparable, V any](cacheBuilder *CacheBuilder[K, V]) *LRUCache[K, V] {
+	return &LRUCache[K, V]{
+		baseCache: baseCache{
+			size: cacheBuilder.size,
+		},
+		m:            make(map[K]lruItem[V]),
+		evictionList: list.New(),
+	}
 }
