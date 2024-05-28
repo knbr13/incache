202f1b9d43c7353ccd7295b653d4320cc84081f6
knbr13 <abdullahalaadine63@gmail.com>
2024-05-29 02:51:38 +0300

    create NewLFU func

--- lfu_cache.go
+++ lfu_cache.go
@@ -11,3 +11,11 @@ type LFUCache[K comparable, V any] struct {
 	m            map[K]*list.Element
 	evictionList *list.List
 }
+
+func NewLFU[K comparable, V any](size uint) *LFUCache[K, V] {
+	return &LFUCache[K, V]{
+		size:         size,
+		m:            make(map[K]*list.Element),
+		evictionList: list.New(),
+	}
+}
